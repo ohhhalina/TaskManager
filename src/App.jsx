4ebd74ab59dd95c6dcfa6c187e4cbbb8b9de9944
Alina Akchurina <ohhalina13@gmail.com>
2026-01-03 21:22:40 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import Login from "./pages/Login"
+import {Routes, Route, Navigate, Outlet, useLocation} from 'react-router-dom'
+import Toaster from "sonner"
+import TaskDetails from "./pages/TaskDetails"
+import Tasks from "./pages/Tasks"
+import Trash from "./pages/Trash"
+import Users from "./pages/Trash"
+import Dashboard from "./pages/Dashboard"
 
 function App() {
 
   return (
-    <Login />
+     <main className='w-full main-h-screen bg-[#f3f4f6]'>
+      <Routes>
+      <Route element={<Layout/>}>
+        <Route path="/" element={<Navigate to="dashboard"/>}/>
+        <Route path="/dashboard >" element={<Dashboard />} />
+        <Route path="/tasks >" element={<Tasks />} />
+        <Route path="/completed/:status >" element={<Tasks />} />
+        <Route path="/in-progress/:status >" element={<Tasks />} />
+        <Route path="/todo/:status >" element={<Tasks />} />
+        <Route path="/team >" element={<Users />} />
+        <Route path="/trashed >" element={<Trash />} />
+        <Route path="/task/:id >" element={<TaskDetails />} />
+      </Route>
+
+      <Route path="/log-in" element={<Login />} />
+      </Routes>
+
+      <Toaster richColors />
+    </main>
   )
 }
 
