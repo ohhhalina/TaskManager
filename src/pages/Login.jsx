0559d1e6530e642ebbf2b5898956d43a3393ff0e
Alina Akchurina <ohhalina13@gmail.com>
2026-01-09 00:20:54 +0300

    commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,6 +12,9 @@ const Login = () => {
 
 const navigate = useNavigate();
 
+const submitHandler = async (data) => {
+  console.log("submit")
+}
 useEffect(() => {
   user && navigate("/dashboard")
 }, [user] )
@@ -20,7 +23,8 @@ useEffect(() => {
    <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]">
     <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row item-center justify-center">
   {/* left side */}
-  <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
+  <div className="h-full w-full lg:w-2/3 flex flex-col
+  items-center justify-center">
   <div className="w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5
   md:gap-y-10 2xl:-mt-20">
     <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300
@@ -38,7 +42,23 @@ useEffect(() => {
   </div>
 
   {/* right side */}
-  <div></div>
+  <div className='w-full md:w-1/3 p-4 md:p-1 flex flex-col
+  justify-center items-center'>
+    <form onSubmit={handleSubmit(submitHandler)}
+    className='form-container w-full md:w-[400px] flex
+    flex-col gap-y-8 bg-white px-10 pt-14 pb-14'
+    >
+      <div className=''>
+        <p className='text-blue-600 text-3xl font-bold
+        text-center'>
+          Welcome back!
+        </p>
+        <span>
+          Keep all your
+        </span>
+      </div>
+    </form>
+  </div>
     </div>
    </div>
   )
