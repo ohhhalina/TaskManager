3380f84db04d343dc7dbe37d8d37fa423ef76490
Alina Akchurina <ohhalina13@gmail.com>
2026-01-08 22:07:00 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Login from "./pages/Login"
-import {Routes, Route, Navigate, Outlet, useLocation} from 'react-router-dom'
+import {Routes, Route, Navigate, Outlet, useLocation, replace} from 'react-router-dom'
 import Toaster from "sonner"
 import TaskDetails from "./pages/TaskDetails"
 import Tasks from "./pages/Tasks"
@@ -7,6 +7,30 @@ import Trash from "./pages/Trash"
 import Users from "./pages/Trash"
 import Dashboard from "./pages/Dashboard"
 
+function Layout () {
+  const user = ""
+
+  const location = useLocation()
+
+  return user ? (
+    <div className="w-full h-screen flex flex-col md:flex-row">
+      <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
+        {/* <Sidebar/> */}
+      </div>
+        {/* <MobileSidebar/> */}
+
+        <div className="flex-1 overflow-y-auto">
+          {/* <Navbar/> */}
+
+          <div className="p-4 2xl:px-10">
+            <Outlet />
+          </div>
+        </div>
+    </div>
+  ): (
+    <Navigate to="/log-in" state={{from: location}} replace/>
+  )
+}
 function App() {
 
   return (
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import {useform} from 'react-hook-form'
 
 const Login = () => {
-  return (
-   <div>
+  const user = "";
+  const {
+    register, 
+    handleSubmit, 
+    formState: {errors},
+  } = useform()
 
+const navigate = useNavigate();
+
+useEffect(() => {
+  user && navigate("/dashboard")
+}, [user] )
+
+  return (
+   <div className="w-full main-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]">
+    <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row item-center justify-center">
+  {/* left side */}
+  <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
+  <div className="w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5
+  md:gap-y-10 2xl:-mt-20">
+    <span className='flex gap-1 py-1 px-3 border rounded-full text text-sm md:text-base bordergray-300
+    text-gray-600'>Manage all your task in one place!</span>
+  </div>
+  </div>
+
+  {/* right side */}
+  <div></div>
+    </div>
    </div>
   )
 }
