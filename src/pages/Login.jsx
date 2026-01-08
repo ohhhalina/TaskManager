c06bd4a49e2171b3a7b5bccebb4d60a6ddf66b94
Alina Akchurina <ohhalina13@gmail.com>
2026-01-08 22:39:07 +0300

    commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
-import {useform} from 'react-hook-form'
+import {useForm} from 'react-hook-form'
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const user = "";
@@ -7,7 +8,7 @@ const Login = () => {
     register, 
     handleSubmit, 
     formState: {errors},
-  } = useform()
+  } = useForm()
 
 const navigate = useNavigate();
 
@@ -16,14 +17,23 @@ useEffect(() => {
 }, [user] )
 
   return (
-   <div className="w-full main-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]">
+   <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]">
     <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row item-center justify-center">
   {/* left side */}
   <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
   <div className="w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5
   md:gap-y-10 2xl:-mt-20">
-    <span className='flex gap-1 py-1 px-3 border rounded-full text text-sm md:text-base bordergray-300
+    <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300
     text-gray-600'>Manage all your task in one place!</span>
+    <p className="flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black
+    text-center text-blue-700">
+      <span>Cloud-Based</span>
+      <span>Task Manager</span>
+    </p>
+
+    <div className='cell'>
+      <div className="circle rotate-in-up-left"></div>
+    </div>
   </div>
   </div>
 
