d237aa2d8fbd5f7c390ec33a883222370afc20ed
Alina Akchurina <ohhalina13@gmail.com>
2026-01-14 22:19:32 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Tasks from "./pages/Tasks"
 import Trash from "./pages/Trash"
 import Users from "./pages/Users"
 import Dashboard from "./pages/Dashboard"
+import Sidebar from "./components/Sidebar";
 
 function Layout () {
   const {user} = useSelector((state) => state.auth)
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import {useForm} from 'react-hook-form'
 
 const Dashboard = () => {
   return (
